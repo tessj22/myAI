fe63e3f129b71270320f4b75890d190c0d30cad1
tessj22 <tess.e.johnson1@gmail.com>
2025-02-17 15:37:50 -0500

    Changed clear button and page title

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -3,8 +3,8 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 export const CHAT_HEADER: string = `Chat with Tess`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by Tess Johnson 2025`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const CLEAR_BUTTON_TEXT: string = `Clear Chat`;
+export const PAGE_TITLE: string = `Chat with Tess`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
