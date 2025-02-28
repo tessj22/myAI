d86e8cb13316cbb34378b57eb38aca67c3393849
tessj22 <tess.e.johnson1@gmail.com>
2025-02-28 16:14:52 -0500

    Changed titles
    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Guess That Quote!`;
+export const CHAT_HEADER: string = `All About Sleep!`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by Tess Johnson 2025`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Chat`;
-export const PAGE_TITLE: string = `Guess That Quote!`;
+export const PAGE_TITLE: string = `All About Sleep!`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
