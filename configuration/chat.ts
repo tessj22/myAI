2a062b75e43743af3b14e03368da0695226421ea
tessj22 <tess.e.johnson1@gmail.com>
2025-02-28 21:29:10 -0500

    Changed initial message
    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,8 +1,8 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}! ${OWNER_NAME} has set me up to help you dive into the wonderful and important world of sleep! We all need it,
-but most of us could benefit from learning more about it and how to achieve better sleep. Please ask me any questions that come to mind about your own sleep habits or
-questions, and I'll do my best to help you out!`;
+export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}! ${OWNER_NAME} has set me up to help you get the best sleep of your life! Feel free to tell me a little about
+your sleep woes, who you're shopping for (yourself, kids, etc.), and any preferences you may have for sleep aid supplements (ingredients, flavor, strength, etc.). I'll
+work with you to help you find the perfect supplement to get you the best sleep possible!`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
