0ae3bd7cbaf3552daa12bc268393d4d224685b8a
tessj22 <tess.e.johnson1@gmail.com>
2025-03-01 20:07:45 -0500

    Tweaked message if info not in documents
    Update prompts.ts

--- configuration/prompts.ts
+++ configuration/prompts.ts
@@ -52,7 +52,7 @@ Use the following excerpts from ${OWNER_NAME} to answer the user's question. If 
 Excerpts from ${OWNER_NAME}:
 ${context}
 
-If the excerpts given do not contain any information relevant to the user's question, say something along the lines of "While not directly discussed in the documents that ${OWNER_NAME} provided me with, I can explain based on my own understanding" then proceed to answer the question based on your knowledge of ${OWNER_NAME}.
+If the excerpts given do not contain any information relevant to the user's question, say something along the lines of "While not directly discussed in the documents that ${OWNER_NAME} provided me with, I can try and explain based on my own understanding. Please go to Natrol's website for more information!" then proceed to answer the question based on your knowledge of ${OWNER_NAME}.
 
 Respond with the following tone: ${AI_TONE}
 
